6d79d0144c03fa91c0ea53b56a9f91c26b7d4fe0
Thuy Giang <gthuyduong@yahoo.com>
2014-02-24 22:17:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThuyCreative4::Application.config.secret_key_base = '72c8380ed413282399aed6bb04b1418589e82ec52dff1e4408be1598f4f812b390ad7704b280ca399200f568032a6cd93a08eb6e36e936f9f13440978f5cecc4'
